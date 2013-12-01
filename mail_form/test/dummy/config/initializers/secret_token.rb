01ce24889f35fed38bfd0347c4de017fbddecc55
Ben Lewis <bennlewis@gmail.com>
2013-12-01 14:24:09 -0700

    completed ch1 - pdf generator

---
+++ mail_form/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '935ebedd6523edc62ac0a4387159f44445e3b4d8dcb01ccf870eff1af6e9c3e1ddeb0566f95cea77a13cb2f624dc2b77d2ab1616564572060818076a5db2fe73'
