0c6760ab77302ffb6c1b3d76e68592fea54719ca
Ben Lewis <bennlewis@gmail.com>
2013-12-03 07:11:06 -0700

    built rb and string handlers

---
+++ handlers/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '8f00b5ae36f36a7ee8b0143f2bee8b3b70c0a2e24b2bed761fe1628b28e63bc0013c66c3723bb5fd7285175b4ff8f2d88d5f0ab8c36a0bcbd598def715166216'
---
+++ handlers/test/integration/rendering_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class RenderingTest < ActionDispatch::IntegrationTest
+  test ".rb template handler" do
+    get "/handlers/rb_handler"
+    expected = "This is my first <b>template handler</b>!"
+    assert_match expected, response.body
+  end
+
+  test ".string template handler" do
+    get "/handlers/string_handler"
+    expected = "Congratulations! You just created another template handler!"
+    assert_match expected, response.body
+  end
+
+  test ".md template handler" do
+    get "handlers/md_handler"
+    expected = "<p>RDiscount is <em>cool</em> and <strong>fast</strong>!</p>"
+    assert_match expected, response.body
+  end
+end
